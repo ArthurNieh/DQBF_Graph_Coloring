ea90676b8db917c13eb9132cdb4b64839d98e132
ArthurNieh <nieharthur@gmail.com>
2024-12-12 01:30:30 +0800

    [blif_gen] same node same color

--- sudoku/blif_gen_auto.py
+++ sudoku/blif_gen_auto.py
@@ -35,7 +35,40 @@ def add_main_model():
         blif_lines.append("D" + str(i) + "=d" + str(i) + " ")
     blif_lines.append("O_equal=ncolor\n")
 
-    blif_lines.append(".subckt imply I0=e I1=ncolor O=f\n")
+    blif_lines.append(".subckt onehot" + str(c_num) + " ")
+    for i in range(c_num):
+        blif_lines.append("I" + str(i) + "=c" + str(i) + " ")
+    blif_lines.append("O=conehot\n")
+
+    blif_lines.append(".subckt onehot" + str(d_num) + " ")
+    for i in range(d_num):
+        blif_lines.append("I" + str(i) + "=d" + str(i) + " ")
+    blif_lines.append("O=donehot\n")
+
+    blif_lines.append(".subckt imply I0=e I1=ncolor O=diffcolor\n")
+
+    blif_lines.append(".subckt and2 I0=conehot I1=donehot O=conehotdonehot\n")
+
+    # not same node
+    blif_lines.append(".subckt UneqV" + str(u_num) + " ")
+    for i in range(u_num):
+        blif_lines.append("U" + str(i) + "=u" + str(i) + " ")
+    for i in range(v_num):
+        blif_lines.append("V" + str(i) + "=v" + str(i) + " ")
+    blif_lines.append("O_equal=notsamenode\n")
+
+    # not same color
+    blif_lines.append(".subckt UneqV" + str(c_num) + " ")
+    for i in range(c_num):
+        blif_lines.append("U" + str(i) + "=c" + str(i) + " ")
+    for i in range(d_num):
+        blif_lines.append("V" + str(i) + "=d" + str(i) + " ")
+    blif_lines.append("O_equal=notsamecolor\n")
+
+    blif_lines.append(".subckt imply I0=notsamecolor I1=notsamenode O=notsamecolornode\n")
+
+    blif_lines.append(".subckt and2 I0=diffcolor I1=conehotdonehot O=temp1\n")
+    blif_lines.append(".subckt and2 I0=notsamecolornode I1=temp1 O=f\n")
 
     blif_lines.append(".end\n\n")
 
@@ -49,7 +82,7 @@ def add_implicit_graph():
     blif_lines.append("\n")
     blif_lines.append(".outputs E\n")
     
-    blif_lines.append(".subckt UneqV ")
+    blif_lines.append(".subckt UneqV" + str(u_num) + " ")
     for i in range(u_num):
         blif_lines.append("U" + str(i) + "=U" + str(i) + " ")
     for i in range(v_num):
@@ -110,12 +143,13 @@ def add_color_not_equal():
     blif_lines.append(".end\n\n")
     return
 
-def add_and_gate():
-    blif_lines.append(".model and2\n")
-    blif_lines.append(".inputs I0 I1\n")
+### All fundamental gates
+def add_not_gate():
+    blif_lines.append(".model not\n")
+    blif_lines.append(".inputs I\n")
     blif_lines.append(".outputs O\n")
-    blif_lines.append(".names I0 I1 O\n")
-    blif_lines.append("11 1\n")
+    blif_lines.append(".names I O\n")
+    blif_lines.append("0 1\n")
     blif_lines.append(".end\n\n")
     return
 
@@ -167,8 +201,8 @@ def add_or_num(num):
     blif_lines.append(".end\n\n")
     return
 
-def add_and_half_u_num():
-    num = int(u_num/2)
+def add_and_num(num):
+    # num = int(u_num/2)
     blif_lines.append(".model and" + str(num) + "\n")
     blif_lines.append(".inputs ")
     for i in range(num):
@@ -183,21 +217,21 @@ def add_and_half_u_num():
     blif_lines.append(".end\n\n")
     return
 
-def add_UnequivV():
+def add_UnequivV(num):
     # vector U and V are not equivalent
-    blif_lines.append(".model UneqV\n")
+    blif_lines.append(".model UneqV" + str(num) + "\n")
     blif_lines.append(".inputs ")
-    for i in range(u_num):
+    for i in range(num):
         blif_lines.append("U" + str(i) + " ")
-    for i in range(v_num):
+    for i in range(num):
         blif_lines.append("V" + str(i) + " ")
     blif_lines.append("\n")
     blif_lines.append(".outputs O_equal\n")
     
-    for i in range(u_num):
+    for i in range(num):
         blif_lines.append(".subckt nequiv I0=U" + str(i) + " I1=V" + str(i) + " O=unequal" + str(i) + "\n")
-    blif_lines.append(".subckt or" + str(u_num))
-    for i in range(u_num):
+    blif_lines.append(".subckt or" + str(num))
+    for i in range(num):
         blif_lines.append(" I" + str(i) + "=unequal" + str(i))
     blif_lines.append(" O=O_equal\n")
     blif_lines.append(".end\n\n")
@@ -235,7 +269,7 @@ def add_onehot(num):
         blif_lines.append("I" + str(i) + " ")
     blif_lines.append("O\n")
     for i in range(num):
-        line = "-" * num + " 1\n"
+        line = "0" * num + " 1\n"
         line = line[:i] + '1' + line[i+1:]
         blif_lines.append(line)
 
@@ -243,17 +277,21 @@ def add_onehot(num):
     return
 
 def add_subcircuit_model():
+    add_not_gate()
     add_or_num(2)
-    add_and_gate()
+    add_and_num(2)
     add_imply_gate()
     add_equiv_gate()
     add_nequiv_gate()
     add_or_num(u_num)
+    add_UnequivV(u_num)
     if u_num != c_num : 
         add_or_num(c_num)
-    add_and_half_u_num()
-    add_UnequivV()
+        add_UnequivV(c_num)
+    add_and_num(int(u_num/2))
+    
     add_UxequivVx()
+    add_onehot(c_num)
     return
 
 if __name__ == "__main__":
