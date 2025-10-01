2a4bc03f9bd1da42f8c2fde1e117dd85f3253de6
ArthurNieh <nieharthur@gmail.com>
2025-10-01 21:31:09 +0800

    add dqbdd script

--- all_simplified_iscas_dqbdd.sh
+++ all_simplified_iscas_dqbdd.sh
@@ -3,11 +3,11 @@
 # Load FF table
 source ./ff_table.sh
 
-mkdir -p iscas89/result/clique_simplified/
+mkdir -p iscas89/result/simplified_dqbdd/
 rm iscas89/benchmarks/*simplified.*
 
-for (( FF_num=29; FF_num<=32; FF_num=FF_num+1 )); do
-	for (( i=3; i<=8; i=i+1 )); do
+for (( FF_num=3; FF_num<=3; FF_num=FF_num+1 )); do
+	for (( i=2; i<=2; i=i+1 )); do
 	# Create an array of sorted benchmarks by numeric instance number
 		mapfile -t sorted_benches < <(
 			for f in iscas89/benchmarks/s*.bench; do
@@ -27,7 +27,11 @@ for (( FF_num=29; FF_num<=32; FF_num=FF_num+1 )); do
 			ins=${bench#"$prefix"}
 			ins=${ins%"$suffix"}
 
-			output="iscas89/result/clique_simplified/${ins}_Clique${i}_FF${FF_num}"
+			output="iscas89/result/simplified_dqbdd/${ins}_C${i}_FF${FF_num}"
+
+			echo "${ins}"
+			echo "color=${i}"
+			echo "FF_tokeep=${FF_num}"
 
 			ff_threshold=${ff_table[$ins]}
 			if [ -z "$ff_threshold" ]; then
@@ -41,14 +45,10 @@ for (( FF_num=29; FF_num<=32; FF_num=FF_num+1 )); do
 
 			echo $bench &> "$output"
 			echo "${ins}" &>> "$output"
-			echo "clique_size=${i}" &>> "$output"
+			echo "color=${i}" &>> "$output"
 			echo "FF_tokeep=${FF_num}" &>> "$output"
 
-			echo "${ins}"
-			echo "clique_size=${i}"
-			echo "FF_tokeep=${FF_num}"
-
-			./my_clique_simplified_iscas.sh "$ins" "$i" "$FF_num" &>> "$output"
+			./my_simplified_iscas_dqbdd.sh "$ins" "$i" "$FF_num" &>> "$output"
 
 			rm iscas89/benchmarks/*simplified.*
 		done
---
+++ my_clique_simplified_iscas_dqbdd.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Auto Generate iscas.blif and run the DQBF solver
+# Author: Arthur Nieh
+# Date: 2025/9/19
+# Usage: ./iscas.sh
+
+instance=$1
+clique_size=$2
+FF_tokeep=$3
+if [ -z "$instance" ]; then
+    echo "Usage: $0 <iscas89_instance> <clique_size> <FF_tokeep>"
+    exit 1
+fi
+if [ -z "$clique_size" ]; then
+    echo "Usage: $0 <iscas89_instance> <clique_size> <FF_tokeep>"
+    exit 1
+fi
+if [ -z "$FF_tokeep" ]; then
+    echo "Usage: $0 <iscas89_instance> <clique_size> <FF_tokeep>"
+    exit 1
+fi
+
+echo "######################################################"
+echo "Solving $instance for $clique_size size clique\n"
+
+# Generate the blif file
+cd ./iscas89
+python3 simplify_bench.py "$instance" "$FF_tokeep"
+python3 bench_to_blif.py "${instance}_simplified"
+
+start2=`date +%s.%N`
+python3 blif_gen_iscas_clique.py "${instance}_simplified" "$clique_size"
+# this will generate lsfr.blif
+
+# Generate DQDIMACS for the DQBF solver
+# echo "######################################################"
+echo "Generate DQDIMACS file"
+    ## this part is written at /home/arthur/program/abc/dqbf
+    ## the abc have been modified to support DQBF
+cd ../abc/dqbf
+python3 convert_to_cnf_clique.py "../../iscas89/sample/${instance}_simplified_clique.blif"
+
+start3=`date +%s.%N`
+# Run the DQBF solver
+cd ../../bin/
+echo "======================================="
+echo "Run DQBDD"
+timeout 1h ./dqbdd ../abc/dqbf/dqdimacs.txt
+
+end2=`date +%s.%N`
+
+runtime2=$( echo "$end2 - $start2" | bc -l )
+echo -e "\nRuntime for DQBF was $runtime2 seconds.\n"
+runtime3=$( echo "$end2 - $start3" | bc -l )
+echo -e "\nRuntime for DQBDD was $runtime3 seconds.\n"
