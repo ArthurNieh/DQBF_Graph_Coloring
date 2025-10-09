bbe8ff37240d4421a11e32759b8385b095532bc3
ArthurNieh <nieharthur@gmail.com>
2025-10-10 00:22:29 +0800

    add hamiltonian dqbdd script

---
+++ all_hamiltonian_simplified_iscas_dqbdd.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Load FF table
+source ./ff_table.sh
+
+dir_path="iscas89/result/hamiltonian_simplified_dqbdd/"
+
+mkdir -p "$dir_path"
+rm iscas89/benchmarks/*simplified.*
+
+for (( FF_num=3; FF_num<=3; FF_num=FF_num+1 )); do
+	# Create an array of sorted benchmarks by numeric instance number
+	mapfile -t sorted_benches < <(
+		for f in iscas89/benchmarks/s*.bench; do
+			num=$(basename "$f" .bench | sed 's/^s//')
+			echo "$num $f"
+		done | sort -n | awk '{print $2}'
+	)
+
+	for bench in "${sorted_benches[@]}"; do	
+		echo "##############################"
+		# echo "##############################"
+		echo $bench;
+		echo "Current time: $(date)"
+
+		prefix="iscas89/benchmarks/"
+		suffix=".bench"
+		ins=${bench#"$prefix"}
+		ins=${ins%"$suffix"}
+
+		file_name="${dir_path}/hamiltonian_${ins}_FF${FF_num}"
+
+		echo "${ins}"
+		echo "FF_tokeep=${FF_num}"
+
+		ff_threshold=${ff_table[$ins]}
+		if [ -z "$ff_threshold" ]; then
+			echo "No FF number found for $ins, skipping..."
+			continue
+		fi
+		if (( ff_threshold < FF_num )); then
+			echo "$ins skip"
+			continue
+		fi
+
+		echo $bench &> "$file_name"
+		echo "${ins}" &>> "$file_name"
+		echo "FF_tokeep=${FF_num}" &>> "$file_name"
+
+		./my_hamiltonian_simplified_iscas_dqbdd.sh "$ins" "$FF_num" &>> "$file_name"
+
+		rm iscas89/benchmarks/*simplified.*
+	done
+done
